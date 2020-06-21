a00a1304b3274a86c61b6815e6767b289aaa3efa
zuoanlang <1757192208@qq.com>
2020-06-21 09:17:36 +0800

    react-admin init

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+/**
+ *登录路由组件
+ */
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                Login
+            </div>
+        );
+    }
+}
+
+export default Login;
