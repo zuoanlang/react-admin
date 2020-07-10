be2eacfc463cb4d04a32f7ccf999d74c5380daa6
yangkang <yangkang@souche.com>
2020-07-10 18:34:51 +0800

    feat:角色管理

--- src/pages/role/role.jsx
+++ src/pages/role/role.jsx
@@ -1,14 +1,89 @@
 import React, {Component} from 'react';
+import {Card, Button, Table} from 'antd'
 
 /**
  * 角色路由
  */
 class Role extends Component {
+
+    state = {
+        roles: [
+            {
+                name:'张三',
+                create_time:'123',
+                auth_time:'123',
+                auth_name:'admin'
+            },
+            {
+                name:'张三',
+                create_time:'123',
+                auth_time:'123',
+                auth_name:'admin'
+            },
+            {
+                name:'张三',
+                create_time:'123',
+                auth_time:'123',
+                auth_name:'admin'
+            }
+        ],
+        loading: false
+    }
+
+    /**
+     * 初始化Columns
+     */
+    initColumns = () => {
+        this.columns = [
+            {
+                title: '角色名称',
+                dataIndex: 'name'
+            },
+            {
+                title: '创建时间',
+                dataIndex: 'create_time'
+            },
+            {
+                title: '授权时间',
+                dataIndex: 'auth_time'
+            },
+            {
+                title: '授权人',
+                dataIndex: 'auth_name'
+            },
+        ]
+    }
+
+    UNSAFE_componentWillMount(): void {
+        this.initColumns()
+    }
+
+
     render() {
+        const {roles, loading} = this.state
+        const title = (
+            <span>
+                <Button type='primary' style={{marginRight: 10}}>创建角色</Button>
+                <Button type='primary' disabled>创建角色权限</Button>
+            </span>
+        )
         return (
-            <div>
-                Role
-            </div>
+            <Card title={title}>
+                <Table
+                    bordered
+                    rowKey='_id'
+                    dataSource={roles}
+                    columns={this.columns}
+                    loading={loading}
+                    pagination={{
+                        defaultPageSize: 5,
+                        showQuickJumper: true
+                    }}
+                    rowSelection={{
+                        type:'radio'
+                    }}
+                />
+            </Card>
         );
     }
 }
